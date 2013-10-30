68d4530812c8f6376eaa32948d6267e3c1fe01c5
patbenatar <nick@gophilosophie.com>
2013-10-30 14:31:44 -0700

    No need to require fakefs manually, Bundler is already requiring it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,6 @@ Bundler.require
 
 require "stairs"
 
-require "fakefs"
-
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
